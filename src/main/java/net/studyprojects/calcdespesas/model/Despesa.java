1f08e54365ee5ba74ec63a3f97d46d2b605ff31e
Paulo Ohno <pauloohnogit@gmail.com>
2013-04-08 18:40:35 -0300

    Atualizacao da entidade Despesa.

--- src/main/java/net/studyprojects/calcdespesas/model/Despesa.java
+++ src/main/java/net/studyprojects/calcdespesas/model/Despesa.java
@@ -1,5 +1,44 @@
 package net.studyprojects.calcdespesas.model;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Despesa {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private double fracao;
+
+	private BigDecimal valor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getFracao() {
+		return fracao;
+	}
+
+	public void setFracao(double fracao) {
+		this.fracao = fracao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
 
 }
